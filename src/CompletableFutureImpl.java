e6f46d105b2a72456dd9be74da1fd35ff0fdb3d9
Kumar Gaurav <gaurav.akumar07@gmail.com>
2026-08-22 18:20:46 +0530

    changes for Completable with three methods

---
+++ src/CompletableFutureImpl.java
@@ -0,0 +1,63 @@
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureImpl {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        CompletableFuture<String> customer = CompletableFuture.supplyAsync(()  -> {
+                sleep(2000);
+                return "Customer OK";
+        });
+        CompletableFuture<String> inventory = CompletableFuture.supplyAsync(()  -> {
+                sleep(2000);
+                return "Inventory OK";
+        });
+
+        CompletableFuture<String> payment = CompletableFuture.supplyAsync(()  -> {
+                sleep(5000);
+                return "Payment OK";
+        });
+
+        CompletableFuture<String> customerInventory =
+        customer.thenCombine(
+                inventory,
+                (customerResult, inventoryResult) ->
+                        customerResult + " | " + inventoryResult
+        );
+        CompletableFuture<String> finalResult =
+        customerInventory.thenCombine(
+                payment,
+                (customerInventoryResult, paymentResult) ->
+                        customerInventoryResult +
+                        " | " +
+                        paymentResult
+        );
+        System.out.println(finalResult.join());
+        CompletableFuture.allOf(
+                customer,
+                inventory,
+                payment
+        );
+
+        
+long end = System.currentTimeMillis();
+
+System.out.println(
+        "Total time: " +
+        (end - start) +
+        " ms"
+);
+        
+
+    }
+
+    static void sleep(long milliseconds) {
+    try {
+        Thread.sleep(milliseconds);
+    } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+    }
+}
+    
+    
+}
